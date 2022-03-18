4d2f78fa69f13b674171170f1c2aec4f95b1a3a0
theEvilReaper <steffenwx@gmail.com>
2022-03-18 23:45:38 +0100

    Renaming some values and add class header and credit line

--- src/main/java/net/reaper/vulpes/entity/holder/FloatingYHolder.java
+++ src/main/java/net/reaper/vulpes/entity/holder/FloatingYHolder.java
@@ -9,21 +9,49 @@ import org.jetbrains.annotations.NotNull;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
+ * Create a holder instance which has a small change on the y-axis.
+ * The movement distance can be set over the 'start' and 'end' vector
+ * The system moves between the given position vector
+ *
+ * Some credits are also goes to https://github.com/Moulberry for the idea and code explanation in his stream
  * @author theEvilReaper
  * @version 1.0.0
  * @since 1.0.0
  **/
 public class FloatingYHolder extends ItemHolderEntity {
 
-    private static final float hoverTimeOffset = ThreadLocalRandom.current().nextFloat() * 10;
-    private static final float hoverTimeFactor = 8 + ThreadLocalRandom.current().nextFloat() * 4;
-    private static final float hoverDistanceFactor = 0.03f + ThreadLocalRandom.current().nextFloat() * 0.03f;
+    private static final float TIME_OFFSET = ThreadLocalRandom.current().nextFloat() * 10;
+    private static final float TIME_MOVEMENT_FACTOR = 8 + ThreadLocalRandom.current().nextFloat() * 4;
+    private static final float DISTANCE_MOVEMENT_FACTOR = 0.03f + ThreadLocalRandom.current().nextFloat() * 0.03f;
 
-    private static final int START_DELAY_TICKS = 7;
-    private static final int START_INTERP_TICK = 10;
+    private static final int OFFSET_TICKS = 7;
+    private static final int INTERPOLATE_TICK = 10;
 
     private final Vec start;
     private final Vec end;
+
+    private final Vec.Interpolation interpolationType;
+
+    /**
+     * Creates a new instance from the holder entity.
+     * @param instance The instance for the entity
+     * @param spawnPoint The spawn point from the entity
+     * @param item The item for the entity to hold
+     * @param start The start vector for the movement
+     * @param end The end vector for the movement
+     * @param interpolationType THe type for the interpolation
+     */
+    public FloatingYHolder(@NotNull Instance instance,
+                           @NotNull Point spawnPoint,
+                           @NotNull AbstractItem item,
+                           @NotNull Vec start,
+                           @NotNull Vec end,
+                           @NotNull Vec.Interpolation interpolationType) {
+        super(instance, spawnPoint, item);
+        this.start = start;
+        this.end = end;
+        this.interpolationType = interpolationType;
+    }
 
     /**
      * Creates a new instance from the holder entity.
@@ -41,6 +69,7 @@ public class FloatingYHolder extends ItemHolderEntity {
         super(instance, spawnPoint, item);
         this.start = start;
         this.end = end;
+        this.interpolationType = Vec.Interpolation.SMOOTH;
     }
 
     /**
@@ -53,21 +82,25 @@ public class FloatingYHolder extends ItemHolderEntity {
 
         if (isRemoved()) return;
 
-        if (this.getAliveTicks() > START_INTERP_TICK + START_DELAY_TICKS) {
-            var height = (float) Math.sin(this.getAliveTicks() / hoverTimeFactor + hoverTimeOffset) * hoverDistanceFactor;
-            if (this.getAliveTicks() < 40 + START_INTERP_TICK + START_DELAY_TICKS) {
-                height *= (this.getAliveTicks() - START_DELAY_TICKS + START_INTERP_TICK) / 50f;
+        if (this.getAliveTicks() > INTERPOLATE_TICK + OFFSET_TICKS) {
+            var height = (float) Math.sin(this.getAliveTicks() / TIME_MOVEMENT_FACTOR + TIME_OFFSET) * DISTANCE_MOVEMENT_FACTOR;
+            if (this.getAliveTicks() < 40 + INTERPOLATE_TICK + OFFSET_TICKS) {
+                height *= (this.getAliveTicks() - OFFSET_TICKS + INTERPOLATE_TICK) / 50f;
             }
             this.teleport(this.position.withY(end.y() + height));
-        } else if (this.getAliveTicks() == 10 + START_DELAY_TICKS + START_INTERP_TICK) {
+        } else if (this.getAliveTicks() == 10 + OFFSET_TICKS + INTERPOLATE_TICK) {
             setActive(true);
-        } else if (this.getAliveTicks() > START_DELAY_TICKS) {
-            if (this.getAliveTicks() == START_DELAY_TICKS + START_INTERP_TICK) {
+        } else if (this.getAliveTicks() > OFFSET_TICKS) {
+            if (this.getAliveTicks() == OFFSET_TICKS + INTERPOLATE_TICK) {
                 this.getEntityMeta().setHasGlowingEffect(true);
             }
-            var point= start.interpolate(end,
-                    (float)(this.getAliveTicks() - START_DELAY_TICKS) / START_INTERP_TICK, Vec.Interpolation.SMOOTH);
-            this.teleport(this.position.withCoord(point));
+            applyPosition(Vec.Interpolation.SMOOTH);
         }
     }
-}
+
+    private void applyPosition(@NotNull Vec.Interpolation interpolationTyp) {
+        var point = start.interpolate(end,
+                (float)(this.getAliveTicks() - OFFSET_TICKS) / INTERPOLATE_TICK, interpolationTyp);
+        this.teleport(this.position.withCoord(point));
+    }
+ }
