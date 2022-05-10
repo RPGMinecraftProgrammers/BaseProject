8eee55a3bc4ab5c55cd80c5dbf67ee3bd299e9d2
theEvilReaper <steffenwx@gmail.com>
2022-05-10 23:09:33 +0200

    Add coins function

---
+++ src/main/java/net/reaper/vulpes/coins/CoinFunction.java
@@ -0,0 +1,24 @@
+package net.reaper.vulpes.coins;
+
+import net.minestom.server.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ **/
+@FunctionalInterface
+public interface CoinFunction {
+
+    /**
+     * Handles what happen if the player get coins or lose some coins.
+     * @param player The player who is involved in the process
+     * @param coins The amount of coins
+     */
+    void updateCoins(@NotNull Player player, @NotNull UpdateReason updateReason, int coins);
+
+    enum UpdateReason {
+        ADD, REMOVE;
+    }
+}
