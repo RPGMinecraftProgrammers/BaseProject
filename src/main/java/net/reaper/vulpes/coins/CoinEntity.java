45304b0e000e76eb58b5aa059f02848e90b2e279
theEvilReaper <steffenwx@gmail.com>
2022-05-11 22:06:33 +0200

    Cache list and remove sound value

--- src/main/java/net/reaper/vulpes/coins/CoinEntity.java
+++ src/main/java/net/reaper/vulpes/coins/CoinEntity.java
@@ -1,36 +1,33 @@
 package net.reaper.vulpes.coins;
 
-import net.kyori.adventure.key.Key;
-import net.kyori.adventure.sound.Sound;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.GameMode;
 import net.minestom.server.entity.Player;
-import net.minestom.server.entity.metadata.EntityMeta;
 import net.minestom.server.entity.metadata.item.SnowballMeta;
 import net.minestom.server.instance.EntityTracker;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
-import net.minestom.server.network.packet.server.play.CollectItemPacket;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class CoinEntity extends Entity {
 
-    private static final Sound PICKUP_SOUND = Sound.sound(Key.key("minecraft:block.note_block.bell"), Sound.Source.BLOCK, 1f, 3);
     private static final float DEFAULT_SEARCH_DISTANCE = 8;
 
     private final CoinFunction coinFunction;
     private final int coins;
+    private final float searchDistance;
     private Player targetPlayer = null;
 
-    private float searchDistance;
-
     private int ticksToLive = -1;
+
+    private final List<Player> cachedPlayers = new ArrayList<>();
 
     public CoinEntity(@NotNull CoinFunction coinFunction, float searchDistance, int coins) {
         super(EntityType.SNOWBALL, UUID.randomUUID());
@@ -61,14 +58,13 @@ public class CoinEntity extends Entity {
 
         if (targetPlayer == null || targetPlayer.getGameMode() == GameMode.SPECTATOR ||targetPlayer.getInstance() != this.getInstance()) {
             targetPlayer = null;
+            cachedPlayers.clear();
 
-            var players = new ArrayList<Player>();
-
-            this.getInstance().getEntityTracker().nearbyEntities(this.getPosition(), searchDistance, EntityTracker.Target.PLAYERS, players::add);
+            this.getInstance().getEntityTracker().nearbyEntities(this.getPosition(), searchDistance, EntityTracker.Target.PLAYERS, cachedPlayers::add);
 
             float closedDistanceSq = searchDistance * searchDistance;
 
-            for (Player player : players) {
+            for (Player player : cachedPlayers) {
                 float playerDistanceSq = (float) this.getPosition().distanceSquared(player.getPosition());
 
                 if (playerDistanceSq < closedDistanceSq) {
