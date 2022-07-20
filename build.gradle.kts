a781282c8c382cfe10502a4ea2a3bdae366e50ab
theEvilReaper <steffenwx@gmail.com>
2022-07-20 19:18:06 +0200

    Add WorldSeedEngine as new dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     java
+    `java-library`
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "dev.themeinerlp"
@@ -7,7 +9,7 @@ version = "1.0-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17;
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
@@ -15,21 +17,33 @@ repositories {
     mavenCentral()
 }
 
+var minestomVersion = "master-SNAPSHOT"
+
 dependencies {
-    implementation("org.jetbrains:annotations:23.0.0")
-    implementation("com.github.Minestom:Minestom:-SNAPSHOT")
+    api("com.github.WorldSeedGames:WorldSeedEntityEngine:v1.1.2")
+
+    compileOnly("com.github.Minestom:Minestom:$minestomVersion")
     // implementation("de.icevizion.lib:Aves:1.1.0-SNAPSHOT")
 
+    testImplementation("com.github.Minestom:Minestom:$minestomVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
 }
 
 tasks {
     compileJava {
         options.encoding = "UTF-8"
+        options.release.set(17)
+    }
+
+    jar {
+        dependsOn("shadowJar")
+    }
+
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
     }
 }
