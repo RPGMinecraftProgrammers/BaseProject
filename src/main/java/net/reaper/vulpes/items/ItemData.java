3ad217f58d83348719e46d52097ae4719349172e
theEvilReaper <steffenwx@gmail.com>
2022-07-20 20:38:18 +0200

    Add ItemData record

---
+++ src/main/java/net/reaper/vulpes/items/ItemData.java
@@ -0,0 +1,10 @@
+package net.reaper.vulpes.items;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ **/
+public record ItemData(@NotNull String value, boolean action) { }
